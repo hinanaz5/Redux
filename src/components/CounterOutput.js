ae50a93e7c243a8327be412247f4caadbda7c472
hinanaz5 <hina.hrm87@gmail.com>
2022-11-20 11:16:05 +0500

    first commit of redux

---
+++ src/components/CounterOutput.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+export default function CounterOutput() {
+const counter = useSelector((state) => state.counter);
+return <>Counter Value: {counter}</>;
+}
---
+++ src/components/Store.js
@@ -0,0 +1,8 @@
+import { createStore } from 'redux';
+import CounterReducer from './CounterReducer';
+
+
+const store = createStore(CounterReducer);
+
+
+export default store;
